933d1d88a3d8aaf7c41d9216f9bb20f2999316e0
davidpodhola <david@podhola.net>
2018-05-16 13:56:58 +0200

    new properties

--- src/java/org/idempiere/icommon/model/IPO.java
+++ src/java/org/idempiere/icommon/model/IPO.java
@@ -1,5 +1,7 @@
 package org.idempiere.icommon.model;
 
+import java.util.Properties;
+
 // to be used instead of PO
 public interface IPO {
     String[] get_KeyColumns();
@@ -7,4 +9,13 @@ public interface IPO {
     boolean is_new();
     String get_TableName();
     Object get_Value (String columnName);
+
+    int getAD_Client_ID();
+    int getAD_Org_ID();
+
+    Properties getCtx();
+
+    int get_Table_ID();
+
+    int get_ID();
 }
